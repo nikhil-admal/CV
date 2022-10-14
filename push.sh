fbe85b8a618d6f5c9c1317b0798dd409ea5254cc
nikhil-admal <admal@illinois.edu>
2022-10-14 01:41:23 -0500

    creating empty branch, and ignoring pdf

--- push.sh
+++ push.sh
@@ -6,8 +6,7 @@ setup_git() {
 }
 
 commit_website_files() {
-    git branch
-  git checkout -b gh-pages
+  git switch --orphan gh-pages
   git add *.pdf
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
